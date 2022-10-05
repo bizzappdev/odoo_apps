aa3175352c20192e27ab5449c48e8b173c9c8f07
RUS <RUS@bizzappdev.com>
2022-10-05 10:06:18 +0530

    [ADD]New module for no auto save and proper msg before leaving

---
+++ web_no_auto_save/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "No Auto Save",
+    "summary": """
+        Web module extention to give proper msg and do not auto save
+    """,
+    "sequence": 95,
+    "author": "BizzAppDev",
+    "website": "https://www.bizzappdev.com",
+    "category": "web",
+    "version": "16.0.0.0.0",
+    "depends": ["base", "web"],
+    "data": [],
+    "assets": {
+        "web.assets_backend": [
+            "web_no_auto_save/static/src/js/web_no_auto_save.js",
+            "web_no_auto_save/static/src/xml/web_no_auto_save.xml",
+        ],
+    },
+    "license": "Other proprietary",
+    "installable": True,
+}
---
+++ web_no_auto_save/static/src/js/web_no_auto_save.js
@@ -0,0 +1,65 @@
+/** @odoo-module **/
+import {useSetupView} from "@web/views/view_hook";
+import { FormController } from '@web/views/form/form_controller';
+import {ListController} from '@web/views/list/list_controller';
+import {FormStatusIndicator} from '@web/views/form/form_status_indicator/form_status_indicator';
+
+const { useRef, toRaw } = owl;
+
+const oldSetup = FormController.prototype.setup;
+console.log("web_no_auto_save loaded");
+
+const Formsetup = function () {
+    console.log("setup from CIUSTOM");
+    
+    const rootRef = useRef("root");
+    useSetupView({
+        beforeLeave: () => {
+            if (this.model.root.isDirty) {
+                if (confirm("Do you want to save changes Automatically?")){
+                    return this.model.root.save({ noReload: true, stayInEdition: true });
+                }
+                else{
+                    this.model.root.discard();
+                    return true;
+                }
+                //return this.model.root.save({ noReload: true, stayInEdition: true });
+            }
+        },
+    });
+    const result = oldSetup.apply(this, arguments);
+    return result;
+};
+//assign setup to FormController
+FormController.prototype.setup = Formsetup;
+FormStatusIndicator.template = 'web_no_auto_save.FormStatusIndicator';
+
+const ListSuper = ListController.prototype.setup;
+const Listsetup = function () {
+    console.log("setup from List CIUSTOM");
+    
+    useSetupView({
+
+        rootRef: this.rootRef,
+        beforeLeave: () => {
+            const list = this.model.root;
+            const editedRecord = list.editedRecord;
+            console.log("editedRecord",editedRecord);
+            if (editedRecord && editedRecord.isDirty) {
+                if (confirm("Do you want to save changes Automatically?")){
+                    if (!(list.unselectRecord(true))) {
+                        throw new Error("View can't be saved");
+                    }
+                }
+                else{
+                    this.onClickDiscard();
+                    return true;
+                }
+            }
+        },
+    });
+    const result = ListSuper.apply(this, arguments);
+    return result;
+    
+};
+ListController.prototype.setup = Listsetup;
